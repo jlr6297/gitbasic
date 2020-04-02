b078f2523e23fb6710d36416a0ede227214efe8f
jlr6297 <62716761+jlr6297@users.noreply.github.com>
2020-04-02 14:40:29 -0400

    add some computation

---
+++ hello.java
@@ -0,0 +1,12 @@
+public class hello{
+
+      public void doSomethingInteresting(){
+          int a = 10;
+          int b = 20;
+          a = a a + b;
+          }
+          
+      public static void main(String[] args){
+          System.out.println("Hello Git");
+          }
+          }
